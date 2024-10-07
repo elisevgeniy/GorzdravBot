c441846f101d44a0236c6ca44c9618bf07735f52
elisevgeniy <elisevgeniy@gmail.com>
2024-10-07 23:51:17 +0300

    init

---
+++ src/main/java/ru/kusok_piroga/gorzdravbot/GorzdravBotApplication.java
@@ -0,0 +1,13 @@
+package ru.kusok_piroga.gorzdravbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorzdravBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GorzdravBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kusok_piroga/gorzdravbot/GorzdravBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kusok_piroga.gorzdravbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorzdravBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
