6b547091a713d1a6120c66c044e32115afe4dbba
elisevgeniy <elisevgeniy@gmail.com>
2024-10-20 22:03:47 +0300

    fix deleting task

--- src/main/java/ru/kusok_piroga/gorzdravbot/bot/services/TaskDeleteService.java
+++ src/main/java/ru/kusok_piroga/gorzdravbot/bot/services/TaskDeleteService.java
@@ -3,7 +3,7 @@ package ru.kusok_piroga.gorzdravbot.bot.services;
 import io.github.drednote.telegram.core.request.UpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessages;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.kusok_piroga.gorzdravbot.bot.repositories.TaskRepository;
 
@@ -15,16 +15,13 @@ public class TaskDeleteService {
 
     private final TaskRepository repository;
 
-    public void deleteTask(String taskId, UpdateRequest request){
+    public void deleteTask(String taskId, UpdateRequest request) {
         repository.deleteById(Long.parseLong(taskId));
 
         try {
-            request.getAbsSender().execute(DeleteMessages.builder()
+            request.getAbsSender().execute(DeleteMessage.builder()
                     .chatId(request.getChatId())
-                    .messageIds(List.of(
-                            request.getOrigin().getCallbackQuery().getMessage().getMessageId(),
-                            request.getOrigin().getCallbackQuery().getMessage().getMessageId() - 1
-                    ))
+                    .messageId(request.getOrigin().getCallbackQuery().getMessage().getMessageId())
                     .build()
             );
         } catch (TelegramApiException e) {
