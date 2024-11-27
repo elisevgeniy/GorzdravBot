d9f4ea7976feabefa94db3b4712c87fbf589b1ec
elisevgeniy <elisevgeniy@gmail.com>
2024-11-27 19:34:07 +0300

    test[recorder]: add basic tests for ScheduleService

--- src/test/java/ru/kusok_piroga/gorzdravbot/recorder/services/ScheduleServiceTest.java
+++ src/test/java/ru/kusok_piroga/gorzdravbot/recorder/services/ScheduleServiceTest.java
@@ -2,48 +2,70 @@ package ru.kusok_piroga.gorzdravbot.recorder.services;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import ru.kusok_piroga.gorzdravbot.bot.repositories.DialogRepository;
-import ru.kusok_piroga.gorzdravbot.bot.callbacks.repositories.CallbackRepository;
+import ru.kusok_piroga.gorzdravbot.api.models.AvailableAppointment;
 import ru.kusok_piroga.gorzdravbot.domain.models.TaskEntity;
-import ru.kusok_piroga.gorzdravbot.domain.repositories.PatientRepository;
 import ru.kusok_piroga.gorzdravbot.domain.repositories.TaskRepository;
 
 import java.util.Date;
+import java.util.List;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@ComponentScan("ru.kusok_piroga.gorzdravbot.recorder.services")
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class
-})
+@SpringBootTest(classes = ScheduleService.class)
 class ScheduleServiceTest {
 
     @MockBean
-    private TaskRepository taskRepository;
+    TaskRepository taskRepository;
     @MockBean
-    private PatientRepository patientRepository;
+    FetchService fetchService;
     @MockBean
-    private CallbackRepository callbackRepository;
+    NotifyService notifyService;
     @MockBean
-    private DialogRepository dialogRepository;
-
-    @MockBean
-    private FetchService fetchService;
+    RecordService recordService;
 
     @Autowired
     private ScheduleService scheduleService;
 
     @Test
-    void nullNotificationWithEmptyAppointmentListTest() {
+    void success_notification(){
+        TaskEntity task = new TaskEntity();
+
+        doReturn(task).when(taskRepository).save(task);
+        doReturn(true).when(notifyService).needNotify(task);
+        doReturn(true).when(notifyService).notifyToChat(eq(task), any());
+        doReturn(List.of(new AvailableAppointment("","","","","","")))
+                .when(fetchService).getValidAvailableAppointments(task);
+
+        scheduleService.taskProcess(task);
+
+        verify(taskRepository).save(task);
+        assertThat(task.getLastNotify())
+                .isNotNull();
+    }
+
+    @Test
+    void success_record(){
+        TaskEntity task = new TaskEntity();
+
+        doReturn(task).when(taskRepository).save(task);
+        doReturn(false).when(notifyService).needNotify(task);
+        doReturn(true).when(recordService).isTimeToRecord(task);
+        doReturn(true).when(recordService).makeRecord(eq(task), anyList());
+        doReturn(List.of(new AvailableAppointment("123","","","","","")))
+                .when(fetchService).getValidAvailableAppointments(task);
+
+        scheduleService.taskProcess(task);
+
+        verify(recordService).makeRecord(eq(task), anyList());
+    }
+
+    @Test
+    void nullification_notification_if_empty_appointment_list() {
 
         doReturn(emptyList())
                 .when(fetchService)
