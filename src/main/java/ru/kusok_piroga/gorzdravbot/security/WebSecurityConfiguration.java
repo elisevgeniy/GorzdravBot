ba139ff663274fd3aa556fd02b7c73994947429f
elisevgeniy <elisevgeniy@gmail.com>
2025-01-12 22:45:37 +0300

    feat: add actuator metrics with http Basic auth
    Add health and prometheus endpoints

---
+++ src/main/java/ru/kusok_piroga/gorzdravbot/security/WebSecurityConfiguration.java
@@ -0,0 +1,50 @@
+package ru.kusok_piroga.gorzdravbot.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Value("${security.auth.username}")
+    private String username;
+    @Value("${security.auth.password}")
+    private String password;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/**").hasRole("ADMIN")
+                )
+                .httpBasic(withDefaults())
+                .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+}
